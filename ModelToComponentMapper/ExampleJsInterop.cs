bf6222c7fc0f17f377ee63a786c23cccebc5e314
Brian Parker <brian.parker@hotmail.com.au>
2020-10-06 03:29:08 +1100

    Add project files.

---
+++ ModelToComponentMapper/ExampleJsInterop.cs
@@ -0,0 +1,16 @@
+namespace ModelToComponentMapper
+{
+    using System.Threading.Tasks;
+    using Microsoft.JSInterop;
+
+    public class ExampleJsInterop
+    {
+        public static ValueTask<string> Prompt(IJSRuntime jsRuntime, string message)
+        {
+            // Implemented in exampleJsInterop.js
+            return jsRuntime.InvokeAsync<string>(
+                "exampleJsFunctions.showPrompt",
+                message);
+        }
+    }
+}
